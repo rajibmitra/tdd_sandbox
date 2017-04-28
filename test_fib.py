19a8f0c9e3bac2e1a8aa2941572dcdbc9973a902
Tom Buaas <tbuaas@gmail.com>
2017-04-27 18:23:59 -0700

    Create basic fixture for Flask test client

--- test_fib.py
+++ test_fib.py
@@ -2,14 +2,17 @@ import pytest
 import json
 from fib import app
 
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
 # Acceptance tests
-def test_app_root_answers_200_ok():
-    client = app.test_client()
+def test_app_root_answers_200_ok(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_app_root_gives_json_welcome_message():
-    client = app.test_client()
+def test_app_root_gives_json_welcome_message(client):
     response = client.get('/')
     response_dict = json.loads(response.data)
     assert 'message' in response_dict
